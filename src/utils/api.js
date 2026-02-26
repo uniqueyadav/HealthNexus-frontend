fd8a89bedb9b28e70f5fda1e078a954e9c86f73e
Amit Yadav <yadavamit0186@gmail.com>
2026-02-26 12:37:31 +0530

    URL update

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL,
+    baseURL: "https://healthnexus-backend-zip1.onrender.com/api",
 });
 
 export default API;
